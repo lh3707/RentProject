5b0656e6e426f59528dd1e90ce439ef5d9b2328b
hong <hongliu023@qq.com>
2017-11-16 00:30:34 +0800

    step one:finished crawl.py

--- code/rent_proj/crawl.py
+++ code/rent_proj/crawl.py
@@ -9,14 +9,14 @@ url = "http://xa.58.com/yanta/pinpaigongyu/pn/{page}/?minprice=1000_1500"
 # page number ,init 0
 page = 0
 
-csv_file = codecs.open("rent.csv", "w")
+csv_file = open("rent.csv", "w")
 csv_writer = csv.writer(csv_file, delimiter=',')
 
 while True:
 	page += 1
 	print("fetch: ", url.format(page=page))
 	response = requests.get(url.format(page=page))
-	html = BeautifulSoup(response.content, 'lxml')
+	html = BeautifulSoup(response.text, 'lxml')
 	house_list = html.select(".list > li")
 	house_next = html.select(".next")
 
@@ -24,7 +24,7 @@ while True:
 		break
 
 	for house in house_list:
-		house_title = house.select("h2")[0].string.encode("utf8")
+		house_title = house.select("h2")[0].string
 		house_url = urljoin(url, house.select("a")[0]["href"])
 		house_info_list = house_title.split()
 
@@ -36,9 +36,8 @@ while True:
 		# else:
 		# 	house_location = house_info_list[1]
 
-		house_money = house.select(".money")[0].select("b")[0].string.encode("utf8")
+		house_money = house.select(".money")[0].select("b")[0].string
 		csv_writer.writerow([house_title, house_location, house_money, house_url])
-
 
 csv_file.close()
 
--- readme.md
+++ readme.md
@@ -2,3 +2,13 @@
 HongLIU
 
 test add file to github
+
+2017.11.16
+HongLIU
+
+finished crawi.py
+It's used to crawl the imformation about the house which we want to rent, including house name, address, money, url.
+
+finished a part of index.html
+There is something wrong whth the file, it is the same with the example. So, I need to learn how to use gaode map API. It will take some time.
+
